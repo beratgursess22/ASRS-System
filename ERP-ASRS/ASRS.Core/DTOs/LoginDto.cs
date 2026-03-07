5b433b62bef644d07a7d8d75badba4ce2c38c7fc
ibrahim berat <gursesberat0@gmail.com>
2026-03-07 23:28:33 +0300

    added frontend login and accesdenied page and create css file for pages

--- ERP-ASRS/ASRS.Core/DTOs/LoginDto.cs
+++ ERP-ASRS/ASRS.Core/DTOs/LoginDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ASRS.Core.DTOs;
 
 public class LoginDto
 {
-    public string Email { get; set; } = string.Empty; 
-	public string Password { get; set; } = string.Empty;
-	public bool RememberMe { get; set; } = false; // kullanıcının oturumunun uzun süre açık kalmasını sağlamak 
+    [Required(ErrorMessage = "E-posta zorunludur.")]
+    [EmailAddress(ErrorMessage = "Geçerli bir e-posta adresi girin.")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Şifre zorunludur.")]
+    public string Password { get; set; } = string.Empty;
+
+    public bool RememberMe { get; set; } = false; // kullanıcının oturumunun uzun süre açık kalmasını sağlamak
 }
--- ERP-ASRS/ASRS.Core/obj/Debug/net9.0/ASRS.Core.AssemblyInfo.cs
+++ ERP-ASRS/ASRS.Core/obj/Debug/net9.0/ASRS.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASRS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b88a7fa08117bbf3cd231a9b4e1fc306a2990f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a5e1cb65d280a2fef917ebdca9a4560d49391d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASRS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASRS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
